a9626ac266a70bd0e01e75cfd7a2feb6a3dc39ac
Hannah Stahl <hls62@cornell.edu>
2019-09-12 11:00:55 -0500

    Added additional API calls

--- custom-problems/list.js
+++ custom-problems/list.js
@@ -7,14 +7,22 @@ export async function main(event, context) {
     // 'KeyConditionExpression' defines the condition for the query
     // - 'userId = :userId': only return items with matching 'userId'
     //   partition key
+    // 'FilterExpression' defines the filter for the query
+    // - 'categoryId = :categoryId': only return items
+    //    with matching 'categoryId', if categoryId specified
     // 'ExpressionAttributeValues' defines the value in the condition
     // - ':userId': defines 'userId' to be Identity Pool identity id
     //   of the authenticated user
+    // - ':categoryId': path parameter
     KeyConditionExpression: "userId = :userId",
     ExpressionAttributeValues: {
       ":userId": event.requestContext.identity.cognitoIdentityId
     }
   };
+  if (event.pathParameters) {
+    params.FilterExpression = "categoryId = :categoryId";
+    params.ExpressionAttributeValues[":categoryId"] = event.pathParameters.id;
+  }
 
   try {
     const result = await dynamoDbLib.call("query", params);
